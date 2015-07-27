202c47df87d98ce71b98eaf120b651dd5d243a77
AlexVanSpilbeeck <Alex.Van.Spilbeeck@cern.ch>
2015-07-27 12:25:00 +0200

    Calibration values - Emin = 0 GeV

---
+++ Calibrating_values.h
@@ -0,0 +1,224 @@
+double Energy_sector_0_data( double edet ){
+  double alpha = 1.92318;
+  double beta = -0.0259161;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_0_MC( double edet ){
+  double alpha = -0.730796;
+  double beta = 0.317797;
+  double gamma = 1000;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_1_data( double edet ){
+  double alpha = 2.00819;
+  double beta = -0.0380573;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_1_MC( double edet ){
+  double alpha = -1.3294;
+  double beta = 0.416306;
+  double gamma = 834.454;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_2_data( double edet ){
+  double alpha = 1.98646;
+  double beta = -0.0303458;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_2_MC( double edet ){
+  double alpha = 1.4676;
+  double beta = 0.0180652;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_3_data( double edet ){
+  double alpha = 2.06167;
+  double beta = -0.0384663;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_3_MC( double edet ){
+  double alpha = 0.488304;
+  double beta = 0.156357;
+  double gamma = 1000;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_4_data( double edet ){
+  double alpha = 2.00697;
+  double beta = -0.029759;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_4_MC( double edet ){
+  double alpha = 4.30995;
+  double beta = -0.390484;
+  double gamma = 1000;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_5_data( double edet ){
+  double alpha = 2.09649;
+  double beta = -0.0484309;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_5_MC( double edet ){
+  double alpha = 0.382766;
+  double beta = 0.167727;
+  double gamma = 1000;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_6_data( double edet ){
+  double alpha = 1.99214;
+  double beta = -0.029911;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_6_MC( double edet ){
+  double alpha = 0.625638;
+  double beta = 0.145404;
+  double gamma = 503.573;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_7_data( double edet ){
+  double alpha = 2.15417;
+  double beta = -0.0583808;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_7_MC( double edet ){
+  double alpha = 1.55196;
+  double beta = 0.00868576;
+  double gamma = 11.4749;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_8_data( double edet ){
+  double alpha = 1.83836;
+  double beta = -0.0167452;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_8_MC( double edet ){
+  double alpha = 1.24978;
+  double beta = 0.0478111;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_9_data( double edet ){
+  double alpha = 1.81336;
+  double beta = -0.0213583;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_9_MC( double edet ){
+  double alpha = 1.32046;
+  double beta = 0.0235222;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_10_data( double edet ){
+  double alpha = 1.49217;
+  double beta = 0.046574;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_10_MC( double edet ){
+  double alpha = 1.27641;
+  double beta = 0.0301812;
+  double gamma = 18.0054;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_11_data( double edet ){
+  double alpha = 1.27364;
+  double beta = 0.100972;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_11_MC( double edet ){
+  double alpha = 1.0685;
+  double beta = 0.0853667;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_12_data( double edet ){
+  double alpha = -1.46688;
+  double beta = 0.621275;
+  double gamma = 240.224;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_12_MC( double edet ){
+  double alpha = -8.51345;
+  double beta = 1.51411;
+  double gamma = 762.692;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_13_data( double edet ){
+  double alpha = -6.39539;
+  double beta = 1.31186;
+  double gamma = 573.552;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_13_MC( double edet ){
+  double alpha = -14.9799;
+  double beta = 2.37971;
+  double gamma = 1000;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_14_data( double edet ){
+  double alpha = 0.741088;
+  double beta = 0.14242;
+  double gamma = 1000;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_14_MC( double edet ){
+  double alpha = 1.41726;
+  double beta = 0.0110958;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_15_data( double edet ){
+  double alpha = 1.83028;
+  double beta = -0.0233138;
+  double gamma = 1;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
+double Energy_sector_15_MC( double edet ){
+  double alpha = -2.18384;
+  double beta = 0.512559;
+  double gamma = 1000;
+return edet * ( (alpha + beta * log( edet + gamma ) ) ) ; 
+}
+
